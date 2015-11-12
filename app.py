9b5383aac7d578b15869f714ce61dd0f53190086
yoshiya0503 <myon53@gmail.com>
2015-11-13 06:48:15 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+# -*- encoding: utf-8 -*-
+"""
+flask movie app
+sample
+"""
+__author__ = 'Yoshiya Ito <myon53@gmail.com>'
+__version__ = '0.0.1'
+__date__ = '13 Nov 2015'
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def show_movie():
+    return render_template('movie.html')
+
+if __name__ == '__main__':
+    app.run();
