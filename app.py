274e75e01f9c9988e59f465b8e28074f73519747
yoshiya0503 <myon53@gmail.com>
2015-11-13 15:19:16 +0900

    add hls

--- app.py
+++ app.py
@@ -12,9 +12,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/movie')
 def show_movie():
     return render_template('movie.html')
+
+
+@app.route('/hls')
+def show_hls():
+    return render_template('hls.html')
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0');
